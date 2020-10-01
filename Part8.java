db6291e9c1990378ba50269667415acab2407faa
Divyajot Singh Mankan <63389007+divyajot98@users.noreply.github.com>
2020-10-01 23:07:23 +0530

    Added part8.java

---
+++ Part8.java
@@ -0,0 +1,37 @@
+package DataTypes;
+
+public class Part8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //Part8
+		char ch='a';
+		ch=99;
+		System.out.println(ch);
+		
+		//ch=64000;
+		//System.out.println(ch);
+		ch=(char)(ch+2);
+		System.out.println(ch);
+		
+		in=ch;
+		System.out.println(in);
+		
+		//ch=68000;
+		ch=(char)68000;
+		System.out.println(ch);
+		
+		//Part9
+		System.out.println(10+20+"Hello"+10+20);
+		System.out.println(2+' '+5);
+		System.out.println(2+" "+5);
+		System.out.println("Hello"+'\t'+"World");
+		System.out.println("Hello"+"\t"+"World");
+		
+	}
+	
+
+}
